c28a1dd51f18270e18e660a998f9b5b151f3c4cb
Aman Pawar <amanp@iprogrammer.co>
2025-03-04 16:35:37 +0530

    cover image issue fixed

--- src/components/BookCard.js
+++ src/components/BookCard.js
@@ -1,35 +1,47 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import '../styles/BookCard.css';
+import defaultImage from '../assests/No_Cover.jpg'; // Add your default image here
 
 const BookCard = ({ book, user, onToggleRead }) => {
+  
+  const handleImageError = (e) => {
+    e.target.onerror = null; // Prevent infinite loop
+    e.target.src = defaultImage; // Replace with default image
+    e.target.alt = 'No cover available'; // Optional alt text
+  };
 
-    // const defaultImage = 'https://via.placeholder.com/150';
-
-    return (
-        <div className="book-card">
-            <Link to={`/books/${book._id}`} className="book-link"> {/* Add this link */}
-                {book?.coverImage ? (
-                    <img src={book?.coverImage} alt="Book Cover" className="default-cover" />
-                ) : (
-                    <div className="default-cover">
-                        <span>No Cover Available</span>
-                    </div>
-                )}
-                <h3>{book?.title}</h3>
-                <p>{book?.author}</p>
-                <p>{book?.genre}</p>
-                <p>{book?.publicationYear}</p>
-            </Link>
-            {user ? (
-                <button onClick={onToggleRead}>
-                    {book?.read ? 'Mark as Unread' : 'Mark as Read'}
-                </button>
-            ) : (
-                <p style={{ color: 'red' }}>Please log in to mark this book as read/unread.</p>
-            )}
-        </div>
-    );
+  return (
+    <div className="book-card">
+      <Link to={`/books/${book._id}`} className="book-link">
+        {book?.coverImage ? (
+          <img
+            src={book?.coverImage}
+            alt="Book Cover"
+            className="default-cover"
+            onError={handleImageError} // Handle image error
+          />
+        ) : (
+            <img
+            src={defaultImage}
+            alt="Book Cover"
+            className="default-cover"
+          />
+        )}
+        <h3>{book?.title}</h3>
+        <p>{book?.author}</p>
+        <p>{book?.genre}</p>
+        <p>{book?.publicationYear}</p>
+      </Link>
+      {user ? (
+        <button onClick={onToggleRead}>
+          {book?.read ? 'Mark as Unread' : 'Mark as Read'}
+        </button>
+      ) : (
+        <p style={{ color: 'red' }}>Please log in to mark this book as read/unread.</p>
+      )}
+    </div>
+  );
 };
 
 export default BookCard;
