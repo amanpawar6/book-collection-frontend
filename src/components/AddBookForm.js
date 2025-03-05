a8cbeb538b332095ccd5392989186bb3cc00afa5
Aman Pawar <amanp@iprogrammer.co>
2025-03-05 12:46:01 +0530

    Addbook validation added

--- src/components/AddBookForm.js
+++ src/components/AddBookForm.js
@@ -13,43 +13,63 @@ const AddBookForm = () => {
     const { token } = useSelector((state) => state.auth); // Get the logged-in user
     const [book, setBook] = useState({ title: '', author: '', genre: '', publicationYear: '' });
     const [coverImage, setCoverImage] = useState(null);
-    const [fileError, setFileError] = useState(''); // New state for file error
+    const [error, setErrors] = useState(''); // New state for file error
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-        setFileError(''); // Clear previous file error
+        setErrors(''); // Clear previous file error
 
-        if (!coverImage) {
-            setFileError('Please select a cover image.');
-            return;
-        }
+        const errors = validateForm();
+        if (Object.keys(errors).length === 0) {
+            const formData = new FormData();
+            formData.append('title', book.title);
+            formData.append('author', book.author);
+            formData.append('genre', book.genre);
+            formData.append('publicationYear', book.publicationYear);
+            formData.append('coverImage', coverImage);
 
-        const formData = new FormData();
-        formData.append('title', book.title);
-        formData.append('author', book.author);
-        formData.append('genre', book.genre);
-        formData.append('publicationYear', book.publicationYear);
-        formData.append('coverImage', coverImage);
+            try {
+                const response = await postAxiosCall('/add-book', formData, {
+                    headers: { 'Content-Type': 'multipart/form-data', Authorization: token },
+                });
 
-        try {
-            const response = await postAxiosCall('/add-book', formData, {
-                headers: { 'Content-Type': 'multipart/form-data', Authorization: token },
-            });
-
-            dispatch(addBook(response.data));
-            showToast("Book added successfully.", "success");
-            setBook({ title: '', author: '', genre: '', publicationYear: '' });
-            setCoverImage(null);
-        } catch (error) {
-            console.error('Error adding book:', error);
-            let errorMessage = error?.message ? JSON.parse(error?.message) : "";
-            if (errorMessage?.status === 403 || errorMessage?.status === 401) {
-                dispatch(logout());
-                showToast('Session expire, Please login again.', 'error');
-                navigate('/login');
+                dispatch(addBook(response.data));
+                showToast("Book added successfully.", "success");
+                setBook({ title: '', author: '', genre: '', publicationYear: '' });
+                setCoverImage(null);
+            } catch (error) {
+                console.error('Error adding book:', error);
+                let errorMessage = error?.message ? JSON.parse(error?.message) : "";
+                if (errorMessage?.status === 403 || errorMessage?.status === 401) {
+                    dispatch(logout());
+                    showToast('Session expire, Please login again.', 'error');
+                    navigate('/login');
+                }
+                showToast("Failed to add book. Please try again.", "error");
             }
-            showToast("Failed to add book. Please try again.", "error");
+        } else {
+            setErrors(errors);
         }
+    };
+
+    const validateForm = () => {
+        let errors = {};
+        if (!book?.title?.trim()) {
+            errors.title = 'Title is required';
+        }
+        if (!book?.author?.trim()) {
+            errors.author = 'Author is required';
+        }
+        if (!book?.genre?.trim()) {
+            errors.genre = 'Genre is required';
+        }
+        if (!book?.publicationYear?.trim()) {
+            errors.publicationYear = 'Publication Year is required';
+        }
+        if (!coverImage) {
+            errors.coverImage = 'Please select a cover image';
+        }
+        return errors;
     };
 
     return (
@@ -57,15 +77,19 @@ const AddBookForm = () => {
             <h2>ADD BOOK</h2>
             <form className="book-form" onSubmit={handleSubmit}>
                 <input name="title" placeholder="Title" value={book.title} onChange={(e) => setBook({ ...book, title: e.target.value })} />
+                {error?.title && <p className="error">{error?.title}</p>}
                 <input name="author" placeholder="Author" value={book.author} onChange={(e) => setBook({ ...book, author: e.target.value })} />
+                {error?.author && <p className="error">{error?.author}</p>}
                 <input name="genre" placeholder="Genre" value={book.genre} onChange={(e) => setBook({ ...book, genre: e.target.value })} />
+                {error?.genre && <p className="error">{error?.genre}</p>}
                 <input name="publicationYear" placeholder="Publication Year" value={book.publicationYear} onChange={(e) => setBook({ ...book, publicationYear: e.target.value })} />
+                {error?.publicationYear && <p className="error">{error?.publicationYear}</p>}
                 <input
                     type="file"
                     accept="image/*"
                     onChange={(e) => setCoverImage(e.target.files[0])}
                 />
-                {fileError && <p className="error">{fileError}</p>} {/* Display file error */}
+                {error?.coverImage && <p className="error">{error?.coverImage}</p>}
                 <button type="submit">Add Book</button>
             </form>
         </div>
